9ca0acf2ef1193c39a85e96cb35eea5f1e7e5442
ZelTroN-2k3 <zeltron_workgroups@hotmail.com>
2018-11-25 10:02:56 +0100

    Add files via upload

---
+++ translations/fr.php
@@ -0,0 +1,25 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{solidaritegiletsjaunes}prestashop>solidaritegiletsjaunes_fc22f18b732c3c17be06436b701d8c6d'] = 'Gilets Jaunes Solidaires';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>solidaritegiletsjaunes_ac0687bc369a8860c0dff1e97901f5d6'] = 'Affiche un petit (gilet jaune) en haut à gauche des vestes jaunes pour les supporters du guichet';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>solidaritegiletsjaunes_780efedff2229b190b28f6c89feef006'] = 'Êtes-vous sûr de vouloir désinstaller ce module Support Yellow Vests?';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>solidaritegiletsjaunes_f4f70727dc34561dfde1a3c529b6205c'] = 'Réglages';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>solidaritegiletsjaunes_cebd5bbe0ffdecc270a8a324e5a277dd'] = 'Mode live';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>solidaritegiletsjaunes_ea9df7a306e2f8b5af37b67084d0c984'] = 'Utilisez ce module en mode live';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>solidaritegiletsjaunes_00d23a76e43b46dae9ec7aa9dcbebb32'] = 'Activée';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>solidaritegiletsjaunes_b9f5c797ebbf55adccdd8539a65a0241'] = 'Désactivé';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>solidaritegiletsjaunes_cffa70587159989d91e6ce586ea65c14'] = 'Entrez une adresse mail valide';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>solidaritegiletsjaunes_ce8ae9da5b7cd6c3df2929543a9af92d'] = 'Email';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>solidaritegiletsjaunes_dc647eb65e6711e155375218212b3964'] = 'Mot de passe';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>solidaritegiletsjaunes_c9cc8cce247e49bae79f15173ce97354'] = 'Sauvegarder';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>configure_fc22f18b732c3c17be06436b701d8c6d'] = 'Gilets Jaunes Solidaires';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>configure_97ef4c38620de4481c6fdf342838c728'] = 'Voici mon nouveau module générique!';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>configure_13adfa430efbb8fd38d9ca2ffe8cbc83'] = 'Grâce à PrestaShop, j\'ai maintenant un excellent module.';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>configure_e2377297f08a03a493c134d08731fbae'] = 'Je peux le configurer en utilisant le formulaire de configuration suivant.';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>configure_476f0d83e3f181072b9f1e350336bf36'] = 'Ce module va booster vos ventes!';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>configure_5b6cf869265c13af8566f192b4ab3d2a'] = 'Documentation';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>configure_dfc94e8224bbd24aa039a3c79fbb9d4e'] = 'Vous pouvez obtenir une documentation PDF pour configurer ce module';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>configure_78463a384a5aa4fad5fa73e2f506ecfc'] = 'Anglais';
+$_MODULE['<{solidaritegiletsjaunes}prestashop>configure_ad225f707802ba118c22987186dd38e8'] = 'Français';
---
+++ views/templates/hook/solidaritegiletsjaunes.tpl
@@ -0,0 +1,4 @@
+
+<div id="solidaritegiletsjaunes">
+	<img src="{$modules_dir}/solidaritegiletsjaunes/views/img/solidaritegiletsjaunes.png" alt="{l s='Displays a small (yellow vest) top left front-office supporter yellow vests.' mod='solidaritegiletsjaunes'}" title="{l s='Displays a small (yellow vest) top left front-office supporter yellow vests.' mod='solidaritegiletsjaunes'}" width="65" height="65"/>
+</div>
